bc1ce685fe71253a9145c56a23f21b8250a7ba79
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-10-23 20:49:26 +0530

    Basic of Typescript

---
+++ variables.ts
@@ -0,0 +1,19 @@
+let greetings: string = "Hello Dilwar"
+console.log(greetings)
+
+// number
+let userId: number = 1023.9
+
+// boolean
+let isLoggedIn: boolean = true;
+
+// String
+let firstName:string = "Dilawar";
+
+let anyValue:any = 0;
+
+anyValue = "farid";
+
+anyValue = true;
+
+export{}
