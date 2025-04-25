cf530a9607d5737057ee4cc9eabcf44c9ffbb47c
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-04-25 21:41:36 +0530

    Array of bojects

--- ObjectType/array/array-annotation.ts
+++ ObjectType/array/array-annotation.ts
@@ -1,4 +1,6 @@
 let numbers: number[] = [1, 2, 3];
-for (let value of numbers) {
+let names: string[] = ["farid", "dilwar"];
+let students: { name: string; age: number }[] = [{ name: "Farid", age: 10 }];
+for (let value of students) {
   console.log(value);
 }
