837daf6fc02c137acb73f345fc3e7f6f60781380
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-04-25 21:04:16 +0530

    interface

---
+++ ObjectType/interface/interface.ts
@@ -0,0 +1,14 @@
+interface Person {
+  name: string;
+  age: number;
+  //    greet:(message:string):void;
+}
+
+let Farid: Person = {
+  name: "Dilwar hossain",
+  age: 10,
+  //   greet(message: string) {
+  //     console.log(`${message}`);
+  //   },
+};
+console.log(Farid);
---
+++ ObjectType/object/object-type.ts
@@ -0,0 +1,6 @@
+let user: { name: string; age: number } = {
+  name: "Farid",
+  age: 10,
+};
+
+console.log(user.age);
