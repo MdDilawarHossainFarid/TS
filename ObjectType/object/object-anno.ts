8bdcfbafaf59445f81e7509984fb56dfce9b9eb2
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-04-27 20:58:05 +0530

    objectAnnotation

---
+++ ObjectType/object/object-anno.ts
@@ -0,0 +1,29 @@
+let person: {
+  name: string;
+  age: number;
+  isStudent: boolean;
+  address: {
+    vill: string;
+    pin: number;
+  };
+};
+
+person = {
+  name: "Farid",
+  age: 2,
+  isStudent: false,
+  address: {
+    vill: "Jhunka",
+    pin: 123456,
+  },
+};
+
+person = {
+  name: "Dilawar",
+  age: 20,
+  isStudent: true,
+  address: {
+    vill: "bhabta",
+    pin: 123456,
+  },
+};
