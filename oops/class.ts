0b9695ef398e60f1c62604683df9ad7556186641
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-10-26 15:31:24 +0530

    OOPS

---
+++ oops/class.ts
@@ -0,0 +1,21 @@
+class Person{
+    // Properties
+    name: string;
+    age: number;
+
+    // Constructor to initialize properties
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method
+    greet(): string{
+        return `Hello, my name is ${this.name} and i am ${this.age} years old.`;
+    }
+}
+
+// Creating an instance of the class
+const person1 = new Person("Alice", 30);
+
+console.log(person1.greet());
